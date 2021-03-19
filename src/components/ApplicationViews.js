97e558c62ca859835ef6fd5eb1e1e590948b5db1
David Bing <davidb.devops@gmail.com>
2021-03-19 14:02:58 -0500

    implement list and card components for tasks and completed tasks

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,32 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
+import { TaskProvider } from "./task/TaskProvider";
+import { TaskList } from "./task/TaskList";
+import { CompletedTaskList } from "./task/CompletedTaskList";
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          backgroundColor: "lightgoldenrodyellow",
+        }}
+      >
+        {/* Current Tasks View */}
+        <TaskProvider>
+          <Route exact path="/">
+            <TaskList />
+          </Route>
+        </TaskProvider>
+
+        {/* Completed Tasks View */}
+        <TaskProvider>
+          <Route exact path="/completed">
+            <CompletedTaskList />
+          </Route>
+        </TaskProvider>
+      </main>
     </>
-}
+  );
+};
---
+++ src/components/task/Task.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react"
+import { Link } from "react-router-dom"
+import Button from 'react-bootstrap/Button'
+import { TaskContext } from "./TaskProvider.js"
+// import "./Task.css"
+
+export const Task = ({ task }) => {
+
+    const { updateTaskStatus, deleteTask } = useContext(TaskContext)
+
+    return (
+        < section key={`task--${task.id}`} className="taskCard" >
+            <div className="task__name">Title: {task.task_name} </div>
+            <div className="task__description">Content: {task.task_description} </div>
+            <div className="task__creationDate">Creation Date: {task.creation_date} </div>
+            <Link key={task.id} id={task.id} to={{ pathname: `/edit/${task.id}`, state: { selectedTask: task } }} >
+                <Button variant="warning"> Edit Task </Button>
+            </Link>
+            
+            <Button variant="success" onClick={() => updateTaskStatus(task.id)} > Mark As Completed Task </Button>
+            <Button variant="danger" onClick={() => deleteTask(task.id)}> Delete Task </Button>
+        </section >
+    )
+}
